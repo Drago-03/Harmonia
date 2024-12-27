11310b310c05a447482405ede6eb452ab749f7bd
Drago <hitmanagent404@gmail.com>
2024-12-27 22:48:36 +0530

    config file now has activity id and updated client secret

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ import logger from './logger.js';
 
 config();
 
-const requiredEnvVars = ['DISCORD_TOKEN', 'GENIUS_API_KEY', 'CLIENT_ID', 'GUILD_ID'];
+const requiredEnvVars = ['DISCORD_TOKEN', 'GENIUS_API_KEY', 'CLIENT_ID', 'GUILD_ID', 'SPOTIFY_CLIENT_ID', 'SPOTIFY_CLIENT_SECRET'];
 
 export function validateConfig() {
   const missingVars = requiredEnvVars.filter(envVar => !process.env[envVar]);
@@ -17,5 +17,12 @@ export const CONFIG = {
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
   GENIUS_API_KEY: process.env.GENIUS_API_KEY,
   CLIENT_ID: process.env.CLIENT_ID,
-  GUILD_ID: process.env.GUILD_ID
+  GUILD_ID: process.env.GUILD_ID,
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+  ACTIVITY_APP_ID: process.env.ACTIVITY_APP_ID || '773336526917861400',
+  MAX_PARTICIPANTS: {
+    DEFAULT: 5,
+    PREMIUM: 20
+  }
 };
